fe703f1c23471cfa327410c99bd589da7aba3ab1
asztyber <sztyber.anna@gmail.com>
2022-05-09 16:11:43 +0200

    working example

--- network_converter/EpanetConverter.py
+++ network_converter/EpanetConverter.py
@@ -1,22 +1,30 @@
 import wntr
+import matplotlib.pyplot as plt
 
 
 class EpanetConverter():
-    """
-    conversion from Epanet water distribution network structure to structural model
-    .inp file is imported with wntr package
-    method according to:
-    Sarrate, R., Nejjari, F., & Rosich, A. (2012). Sensor placement for fault diagnosis performance maximization in
-    Distribution Networks. 2012 20th Mediterranean Conference on Control and Automation,
-    MED 2012 - Conference Proceedings, 110–115. https://doi.org/10.1109/MED.2012.6265623
+    """  Conversion from Epanet water distribution network structure to structural model
+        .inp file is imported with wntr package
+        conversion method according to:
+        Sarrate, R., Nejjari, F., & Rosich, A. (2012). Sensor placement for fault diagnosis performance maximization in
+        Distribution Networks. 2012 20th Mediterranean Conference on Control and Automation,
+        MED 2012 - Conference Proceedings, 110–115. https://doi.org/10.1109/MED.2012.6265623
+
+    Parameters
+    ----------
+    input_file_name : type
+        Description of parameter `input_file_name`.
+    pressure_sensors : type
+        Description of parameter `pressure_sensors`.
+    flow_sensors : type
+        Description of parameter `flow_sensors`.
+    leaks : type
+        Description of parameter `leaks`.
+
     """
 
-    def __init__(self, input_file_name, pressure_sensors, flow_sensors, leaks=None, add_prefix=True):
-        """
-        Parameters:
-        add_prefix (boolean): if true prefix 'P' (pipe) is added to flow variables
-        and 'J' (junction) to pressure variables, needed to assure unique identifiers in the structural model
-        """
+    def __init__(self, input_file_name, pressure_sensors, flow_sensors, leaks=None,
+                 pressure_prefix='p', flow_prefix='q', leak_prefix='f', sensor_faults=False):
 
         self.wn = wntr.network.WaterNetworkModel(input_file_name)
         self.G = self.wn.get_graph(self.wn)
@@ -25,57 +33,48 @@ class EpanetConverter():
         self.f_cnt = -1
         self.eq_name_map = dict()
         self.f_name_map = dict()
-        self.add_prefix = add_prefix
+        self.faults = []
+        self.pressure_prefix = pressure_prefix
+        self.flow_prefix = flow_prefix
+        self.leak_prefix = leak_prefix
+        self.sensor_faults = sensor_faults
 
         if leaks is None:
             self.leaks = self.wn.node_name_list
         else:
             self.leaks = leaks
 
-        if self.add_prefix:
-            self.sensors = ['P' + s for s in flow_sensors] + ['J' + s for s in pressure_sensors]
-            self.leaks = ['J' + leak for leak in self.leaks]
-        else:
-            self.sensors = flow_sensors + pressure_sensors
-
-        try:
-            self.sensors.sort(key=lambda x: (x[:1], int(x[1:])))
-        except ValueError:
-            print('Unexpected node name convention, sorting failed, node names are expected to be integers\
-                    or integers proceeded by a single letter')
+        flow_sensors.sort()
+        pressure_sensors.sort()
+        self.sensors = [flow_prefix + s for s in flow_sensors] + [pressure_prefix + s for s in pressure_sensors]
 
     def flow_balance_eqs(self):
-        faults = []
         flow_balance_eq = dict()
         for node in self.G.nodes:
-            node_name = node
-            if self.add_prefix:
-                node_name = 'J' + node_name
+            node_name = self.pressure_prefix + node
             eq_name = 'e' + node_name
             self.eq_cnt += 1
             eq = 'e' + str(self.eq_cnt)
             self.eq_name_map[eq] = eq_name
-            if node_name in self.leaks:
-                f_name = 'f' + node_name
+            if node in self.leaks:
+                f_name = self.leak_prefix + node_name
                 self.f_cnt += 1
                 f = 'f' + str(self.f_cnt)
                 self.f_name_map[f] = f_name
-                faults.append(f)
+                self.faults.append(f)
                 fs = [f]
             else:
                 fs = []
-            qis = ['P' + name if self.add_prefix else name for e in list(set(self.G.edges(node)))
+            qis = [self.flow_prefix + name for e in list(set(self.G.edges(node)))
                    for name in self.G.get_edge_data(*e).keys()]
             flow_balance_eq[eq] = qis + fs
 
-        return flow_balance_eq, faults
+        return flow_balance_eq
 
     def pipe_eqs(self):
         pipe_eq = dict()
         for p1, p2, q in self.G.edges:
-            q_name, p1_name, p2_name = q, p1, p2
-            if self.add_prefix:
-                q_name, p1_name, p2_name = 'P' + q, 'J' + p1, 'J' + p2
+            q_name, p1_name, p2_name = self.flow_prefix + q, self.pressure_prefix + p1, self.pressure_prefix + p2
             eq_name = 'e' + q_name
             self.eq_cnt += 1
             eq = 'e' + str(self.eq_cnt)
@@ -92,17 +91,22 @@ class EpanetConverter():
             eq = 'e' + str(self.eq_cnt)
             self.eq_name_map[eq] = eq_name
             sensor_eq[eq] = [s, 'm' + s]
+            if self.sensor_faults:
+                f_name = 'fm' + s
+                sensor_eq[eq].append(f_name)
+                self.f_cnt += 1
+                f = 'f' + str(self.f_cnt)
+                self.f_name_map[f] = f_name
+                self.faults.append(f)
         return sensor_eq
 
     def structural_from_epanet(self):
-        if self.add_prefix:
-            unknown = ['J' + n for n in self.wn.node_name_list] + ['P' + link for link in self.wn.link_name_list]
-        else:
-            unknown = self.wn.node_name_list + self.wn.link_name_list
+        unknown = [self.pressure_prefix + n for n in self.wn.node_name_list]
+        unknown += [self.flow_prefix + link for link in self.wn.link_name_list]
         known = ['m' + s for s in self.sensors]
 
         model = dict()
-        flow_balance_eq, faults = self.flow_balance_eqs()
+        flow_balance_eq = self.flow_balance_eqs()
         model.update(flow_balance_eq)
 
         pipe_eq = self.pipe_eqs()
@@ -111,5 +115,17 @@ class EpanetConverter():
         sensor_eq = self.sensor_eqs()
         model.update(sensor_eq)
 
-        sa_model = {'model': model, 'unknown': unknown, 'known': known, 'faults': faults}
+        sa_model = {'model': model, 'unknown': unknown, 'known': known, 'faults': self.faults}
         return sa_model
+
+
+def network_preview(input_file_name):
+    wn = wntr.network.WaterNetworkModel(input_file_name)
+    print('numer of nodes: ', len(wn.node_name_list))
+    print('number of pipes: ', len(wn.link_name_list))
+    print('node name list: ', wn.node_name_list)
+    print('pipe name list: ', wn.pipe_name_list)
+
+    wntr.graphics.network.plot_network(wn, node_attribute=wn.node_name_list, node_labels=True, link_labels=True,
+                                       node_cmap=['lightgray'], node_size=150)
+    plt.show()
